3b66f3bdc14ff4860572c245ab1ee124bc070745
guzhang <guzhang>
2016-12-13 13:16:10 -0500

    added card validation

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.post('/processTxn', function (request, response) {
    console.log("The incoming txn is:")
    console.log(txn)
    response.writeHead(200, {'Content-Type': 'text/plain'});
-   approvalCode = "ABC123"
+   approvalCode = approval(txn);
    response.end(approvalCode);
        
 })
@@ -22,6 +22,18 @@ var server = app.listen(8081, function () {
 
 })
 
+function approval( txn ) {
+	if (txn.cardNumber.length > 2) {
+		return "ABC123"
+	} else {
+		return "error"
+	}
+}
+
+function settlement( txns ) {
+	// TODO
+}
+
 /*
 Requires the following module installed 
 npm install express --save
@@ -32,5 +44,5 @@ npm install mongodb --save
 */
 /*
 Test with the following curl command (with the endpoint url adjusted):
-curl  -H "Content-Type: application/json" -X POST -d '{"merchant-id":"123456789","customer-name":"Bogus Fake","customer-zip":"08054","card-number":"9876543210","card-expiration":"01/01","card-cvv":"123","txn-timestamp":"Tue, Dec 13, 2016 12:27:11 PM","amount":"100.00"}' http://localhost:8081/processTxn
+curl  -H "Content-Type: application/json" -X POST -d '{"merchantId":"123456789","customerName":"Bogus Fake","customerZip":"08054","cardNumber":"9876543210","cardExpiration":"01/01","cardCVV":"123","timestamp":"Tue, Dec 13, 2016 12:27:11 PM","amount":"100.00"}' http://localhost:8081/processTxn
 */
